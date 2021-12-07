804fc3ca4aec3d701663b5c64a72127a0de29d09
diego.carlos <diego_carlos110@hotmail.com>
2021-12-07 16:33:32 -0300

    Adicionado a classe DataClasse

--- sessao_4/dataClasses/data_classes.py
+++ sessao_4/dataClasses/data_classes.py
@@ -9,8 +9,37 @@ Leia a documentação: https://docs.python.org/pt-br/3/library/dataclasses.html
 """
 
 from dataclasses import dataclass
+from dataclasses import field
+from dataclasses import asdict, astuple
 
-@dataclass
+@dataclass(eq=True, repr=True, order=True, frozen=False, init=True)
 class Pessoa:
     nome: str
-    sobrenome: str
+    sobrenome: str = field(repr=False)
+
+
+    def __post_init__(self):
+        if not isinstance(self.nome, str):
+            raise TypeError(
+                f'Invalide type {type(self.nome).__name__} != str em {self}'
+            )
+
+    # @property
+    # def nome_completo(self):
+    #     return f'{self.nome} {self.sobrenome}'
+
+p1 = Pessoa("E", "2")
+p2 = Pessoa("A", "5")
+p3 = Pessoa("B", "3")
+p4 = Pessoa("C", "4")
+p5 = Pessoa("C", "1")
+
+pessoas = [p1, p2 , p3, p4, p5]
+print(sorted(pessoas, key=lambda pessoa: pessoa.sobrenome, reverse=True))
+# print(p1 == p2)
+
+print(asdict(p1))
+print(astuple(p1))
+
+
+
