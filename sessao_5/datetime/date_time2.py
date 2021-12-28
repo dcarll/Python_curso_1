6cc5a7dd2ee3ab513def403c885625ab8d345c01
diego.carlos <diego_carlos110@hotmail.com>
2021-12-28 13:15:50 -0300

    Adionado modolulos sobre como utilizar datetime 2 - atualizado

--- sessao_5/datetime/date_time2.py
+++ sessao_5/datetime/date_time2.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from locale import setlocale, LC_ALL
 from calendar import mdays
+from calendar import monthrange #pegar os dias da semana
 
 setlocale(LC_ALL, "pt_BT.utf-8")
 dt = datetime.now()
@@ -13,3 +14,10 @@ print(formatacao1)
 print(mdays[1:])
 print(f'Mês: {mes_atual}, \nMês contém: {mdays[mes_atual]} dias')
 
+print(monthrange(2022, 2)) #exibe  número correspondente a semana e quantos dias ele contem, os numero das semana vão de 0 a 6
+print("-"*40)
+
+ultimos_dias_de_meses_do_ano_atual = [
+    monthrange(datetime.now().year, mes)[1] for mes in range(1, 13)
+]
+print(ultimos_dias_de_meses_do_ano_atual)
