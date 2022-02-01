89d73261f5e5ba73b0fa00772629e95c3cfc0629
dcarll <diego_carlos110@hotmail.com>
2022-02-01 20:39:21 -0300

    utilizando banco de dados, banco sqlite3

---
+++ sessao_7_base_dedados/aula_4_agendaDB/main.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+class AgendaBD():
+    def __init__(self, arquivo):
+        self.conn = sqlite3.connect(arquivo)
+        self.cursor = self.conn.cursor()
+
+
+    def inserir(self, nome, telefone):
+        consulta = 'INSERT OR IGNORE INTO agenda (nome, telefone) VALUES (?, ?)'
+        self.cursor.execute(consulta, (nome, telefone))
+        self.conn.commit()
+
+    def editar(self, nome, telefone, id):
+        consulta = 'UPDATE OR IGNORE agenda SET nome=?, telefone=? WHERE id=?'
+        self.cursor.execute(consulta, (nome, telefone, id))
+        self.conn.commit()
+
+    def excluir(self, id):
+        consulta = 'DELETE FROM agenda WHERE id=?'
+        self.cursor.execute(consulta, (id, ))
+        self.conn.commit()
+
+    def listar(self):
+        self.cursor.execute('SELECT * FROM agenda')
+
+        for linha in self.cursor.fetchall():
+            id, nome, telefone = linha
+            print(f'ID: {id}\t',
+                  f'Nome: {nome}\t',
+                  f'\tTelefone: {telefone}')
+
+
+    def buscar(self, valor):
+        consulta = 'SELECT * FROM agenda WHERE nome LIKE ?'
+        self.cursor.execute(consulta, (f'%{valor}%',))
+        for linha in self.cursor.fetchall():
+            id, nome, telefone = linha
+            print(f'ID: {id}\t',
+                  f'Nome: {nome}\t',
+                  f'\tTelefone: {telefone}')
+
+
+    def fechar(self):
+        self.cursor.close()
+        self.conn.close()
+
+if __name__ == '__main__':
+    agenda = AgendaBD('agenda.bd')
+    agenda.buscar('João')
+
